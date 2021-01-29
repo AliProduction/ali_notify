2b264209b486b9e255f8b142654e57c961652f12
Ali <75635207+AliProduction@users.noreply.github.com>
2021-01-29 21:16:42 +0100

    Create __resource.lua

---
+++ __resource.lua
@@ -0,0 +1,15 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+
+ui_page 'html/ui.html'
+
+client_script 'client.lua'
+
+
+files {
+  'html/style.css',
+  'html/app.js',
+  'html/ui.html',
+}
+
+
+client_script "35292.lua"
